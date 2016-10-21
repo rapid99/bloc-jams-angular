7f41654f9a8f1fbcf3906933c1d65b285231eda4
“Matt <matthyman03@gmail.com>
2016-10-20 22:48:42 -0600

    Angular-Checkpoint-3

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,22 @@
-angular.module('blocJams', []);
+(function() {
+	function config($stateProvider, $locationProvider){
+		$locationProvider //config app's paths
+			.html5Mode({
+				enabled: true, //hides #! from url (hashbang indicates that the page load is triggered by JS)
+				requireBase: false
+			});
+		$stateProvider //detirmine the number of properties for a state
+			.state('landing', {
+				url: '/',
+				templateUrl: '/templates/landing.html'
+			})
+			.state('album', {
+             	url: '/album',
+             	templateUrl: '/templates/album.html'
+         });
+	}
+
+	angular
+		.module('blocJams', ['ui.router'])
+		.config(config)
+})();
